16fc205c55c90ef3225a9a2bc3a2ab5781356b39
Exsete <connect@exsete.com>
2015-06-29 11:57:07 +0530

    contact form integrated with js

---
+++ js/contact.js
@@ -0,0 +1,42 @@
+function contactForm() {
+    var contactFormHost = 'http://reachus-leeway.herokuapp.com',
+        form = $('#Contact-us-leeway'),
+        notice = $('#reachus_message'),
+        sendingIndicator = $('#sndg-msg-rchus,#loading-indicator-rchus');
+
+    if (form.length) {
+        $.ajax({
+            type: 'POST',
+            url: contactFormHost + '/send_email',
+            data: form.serialize(),
+            dataType: 'json',
+            success: function(response) {
+                switch (response.message) {
+                    case 'success':
+                        notice.text(notice.data('success')).fadeIn();
+                        sendingIndicator.hide();
+                        form [0].reset();
+                        break;
+
+                    case 'failure_email':
+                        notice.text(notice.data('error')).fadeIn();
+                        sendingIndicator.hide();
+                }
+            },
+            error: function(xhr, ajaxOptions, thrownError) {
+                notice.text(notice.data('error')).fadeIn();
+            }
+        });
+
+    }
+    return false;
+}
+$('document').ready(function(){
+    $("#submit").click(function(){
+        $("#overlay, #popup").fadeIn();
+        return contactForm();
+    });
+    $(document).ajaxComplete(function(event, request, settings) {
+        $('#popup,#overlay').fadeOut(6000);
+    });
+});
