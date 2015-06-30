d273ec399102342ca3aaf04eb384eb3994ab5a94
Exsete <connect@exsete.com>
2015-06-30 12:14:15 +0530

    testing 1.7

--- data/js/fileConv.js
+++ data/js/fileConv.js
@@ -1,18 +1,17 @@
-var resumeBase64 = null,
-    fileType = null,
-    fileName = null,
-    subStr = null;
+var resumeBase64 = null,//to get data from form
+    fileType = null,//to get file mime type
+    fileName = null,//to get file name
+    subStr= ";base64,";//setting search filter
     
 
 function handleFileSelect(element){
-    var fileObj = element.files[0];
-    var fileReader = new FileReader();
-    fileReader.onload = function(returnObj){
-        resumeBase64 = returnObj.target.result;
+    var fileObj = element.files[0];//get file object
+    var fileReader = new FileReader();//set a new file reader
+    fileReader.onload = function(returnObj){//attach onload event of file to the function 
+        resumeBase64 = returnObj.target.result;//get data from file
+        resumeBase64=resumeBase64.substring(resumeBase64.lastIndexOf(subStr)+subStr.length+fileType.length);//filter the string to remove preheaders
     };
-    fileReader.readAsDataURL(fileObj);
-    fileType =fileObj.type;
-    fileName =fileObj.name;
-    subStr= fileType+";base64,";
-    resumeBase64=resumeBase64.substring(resumeBase64.lastIndexOf(subStr)+subStr.length);
+    fileReader.readAsDataURL(fileObj);//read file in base64 format
+    fileType =fileObj.type;//read file type
+    fileName =fileObj.name;//read file name
 }
